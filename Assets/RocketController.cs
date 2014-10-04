38daee6b28846cead6075f9a226434d375879f5f
Austin Feight <afeight@umich.edu>
2014-10-04 14:40:32 -0700

    starting someting

---
+++ Assets/RocketController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+
+	float Vx, Vy, Vz;
+	void Start () {
+		Vx = Vy = Vz = 0.0f;
+	}
+
+	void FixedUpdate () {
+		rigidbody.AddForce (50f, 0.0f, 0.0f);
+	}
+	
+}
